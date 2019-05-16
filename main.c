089159eada8bb02f770cd038e65615db0d1fd463
Kelly Hodis <668@holbertonschool.com>
2019-05-16 07:12:30 +0000

    Condensed parsing functions into one

--- main.c
+++ main.c
@@ -1,81 +1,37 @@
 #include "monty.h"
-#include <string.h>
 /**
-* read - open file and read content into a buffer
+* read_line - open file and read content into a buffer
 * @input_file: filename
 * @buffer: to read the contents of file
 *
-* Return: pointer to buffer
+* Return: pointer to line
 */
 
-char *read(FILE *input_file, char *buffer)
+void read(FILE *input, char *buffer, char *commands[])
 {
-	size_t length = 1024, i;
-
-	buffer = malloc(length);
-	if (!buffer)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	for (i = 0; i < length; i++)
-		buffer[i] = '\0';
-
-	fread(buffer, length, 1, input_file);
-	printf("%s\n", buffer);
-	printf("\nthis is just for now\n");
-	return (buffer);
-}
-
-/**
-* parse_buffer - parse buffer into lines
-* @buffer: buffer to parse
-* @instructions: array to store lines
-*
-* Return: 0 if success or line number if failure
-*/
-
-char *parse_buffer(char *buffer, char *instructions[])
-{
+	size_t i, length = 0;
 	char *token;
-	int i;
+	int line_num = 0, line_length = 0;
+	stack_t *stack = NULL;
 
-	token = strtok(buffer, "\n");
-	for (i = 0; token; i++)
+	line_length = getline(&buffer, &length, input);
+	while (line_length >= 0)
 	{
-		instructions[i] = strdup(token);
-		token = strtok(NULL, "\n");
-	}
-	instructions[i] = NULL;
-	return (instructions[0]);
-}
-
-/**
-* parse_instructions - parse lines of buffer into commands with arguments
-* @instruction: line to parse
-* @commands: array to store commands with arguments
-*
-* Return: pointer to the first command
-*/
-
-char *parse_instructions(char *instruction, char *commands[])
-{
-	char *token;
-	int j;
-
-	if (instruction)
-	{
-		token = strtok(instruction, " ");
-		for (j = 0; token; j++)
+		token = strtok(buffer, "\n\t ");
+		for (i = 0; token; i++)
 		{
-			commands[j] = token;
-			token = strtok(NULL, " ");
+			commands[i] = token;
+			token = strtok(NULL, "\n\t ");
 		}
-		commands[j] = NULL;
+		commands[i] = NULL;
+		line_num++;
+		get_op(commands, &stack, line_num);
+		free(buffer);
+		buffer = NULL;
+		line_length = getline(&buffer, &length, input);
 	}
-	return (commands[0]);
 }
+
 /**
 * main - an interpreter for Monty ByteCodes files
 * @argc: argument count
@@ -88,8 +44,7 @@ int main(int argc, char **argv)
 {
 	FILE *input;
 	char *buffer = NULL;
-	char *instructions[25], *commands[30];
-	int i, j;
+	char *commands[30];
 
 	if (argc == 1)
 	{
@@ -104,24 +59,9 @@ int main(int argc, char **argv)
 			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 			exit(EXIT_FAILURE);
 		}
-		buffer = read(input, buffer);
-		printf("%s\nthis is the buffer in main\n", buffer);
-		instructions[0] = parse_buffer(buffer, instructions);
-		for (i = 0; instructions[i]; i++)
-		{
-			commands[0] = parse_instructions(instructions[i], commands);
-			/* call opcode function */
-			for (j = 0; commands[j]; j++)
-				printf("%s\n", commands[j]);
-		}
-		for (i = 0; instructions[i]; i++)
-			printf("%s\n", instructions[i]);
-		for (i = 0; commands[i]; i++)
-			printf("%s\n", commands[i]);
+
+		read(input, buffer, commands);
 	}
-	for (i = 0; instructions[i]; i++)
-		free(instructions[i]);
-	free(buffer);
 	fclose(input);
 	return (0);
 }
